c3ceb52c07fbd5a9d18a0313fced18f5359ee66b
r3nhat <renosnic@gmail.com>
2019-10-05 15:04:11 -0700

    Process Hollowing Technique Added

--- ProcessInjection/Program.cs
+++ ProcessInjection/Program.cs
@@ -190,49 +190,6 @@ namespace ProcessInjection
             public int HighPart;
         }
         #endregion End of Process Hollowing Structs
-        /*
-        #region Process Hollowing Flags
-        [Flags]
-        public enum CreationFlags
-        {
-            CreateSuspended = 0x00000004,
-            DetachedProcesds = 0x00000008,
-            CreateNoWindow = 0x08000000,
-            ExtendedStartupInfoPresent = 0x00080000
-        }
-
-        [Flags]
-        public enum AllocationType
-        {
-            Commit = 0x1000,
-            Reserve = 0x2000,
-            Decommit = 0x4000,
-            Releae = 0x8000,
-            Reset = 0x80000,
-            Physical = 0x400000,
-            TopDown = 0x100000,
-            WriteWatch = 0x200000,
-            LargePages = 0x20000000,
-            SecCommit = 0x08000000
-        }
-
-        [Flags]
-        public enum MemoryProtection
-        {
-            Execute = 0x10,
-            ExecuteRead = 0x20,
-            ExecuteReadWrite = 0x40,
-            ExecuteWriteCopy = 0x80,
-            NoAccess = 0x01,
-            ReadOnly = 0x02,
-            ReadWrite = 0x04,
-            WriteCopy = 0x08,
-            GuardModifierflag = 0x100,
-            NoCacheModifierflag = 0x200,
-            WriteCombineModifierflag = 0x400
-        }
-        #endregion End of Process Hollowing Flags
-        */
 
         public static byte[] StringToByteArray(string hex)
         {
